f4cbeeaa697feeaa807d059e6b478ccc6a356f7d
Dani Gutiérrez <Guty@MacBook-Pro-de-Jessica.local>
2023-02-19 17:56:32 -0600

    Se agregó Lab5

---
+++ Lab5/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['!**/node_modules/**', './src/pages/*.{js, html}', './public/landingPage/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
