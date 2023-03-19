55bd9be4a17b2f746b52041345f6f4d555d1799c
Dani Gutiérrez <Guty@MacBook-Pro-de-Jessica.local>
2023-03-19 00:02:22 -0600

    Bugs corrected

--- Lab18/app.js
+++ Lab18/app.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
 const csrf = require('csurf');
-const isAuth = require('./util/is-auth');
+const isAuth = require('./util/is-auth.js');
 const session = require('express-session');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', 'views/partials');
+
 app.use(session({
     secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste',
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
+
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const csrfProtection = csrf();
 app.use(csrfProtection);
-
-app.use(cookieParser());
-app.set('view engine', 'ejs');
-app.set('views','views/partials');
-
-app.use(express.static(path.join(__dirname)));
-
 const usersModule = require("./routes/users.routes");
 const homeModule = require("./routes/home.routes");
 const buyModule = require("./routes/buy.routes");
